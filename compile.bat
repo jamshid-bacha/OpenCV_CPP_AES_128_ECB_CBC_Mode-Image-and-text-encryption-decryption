e4952e5f3711a7281052e27e9c374339974f895e
jamshid1255 <jamshid1255@gmail.com>
2020-07-28 16:51:52 +0500

    AES_128_ECB_CBC_Mode-Image-and-text-encryption-decryption

---
+++ compile.bat
@@ -0,0 +1,21 @@
+g++ ^
+                -g ^
+                %1 ^
+                -o ^
+                %2 ^
+                -I C:\OpenCV\include ^
+                -L C:\OpenCV\x64\mingw\bin ^
+                -llibopencv_calib3d411 ^
+                -llibopencv_core411 ^
+                -llibopencv_dnn411 ^
+                -llibopencv_features2d411 ^
+                -llibopencv_flann411 ^
+                -llibopencv_highgui411 ^
+                -llibopencv_imgcodecs411 ^
+                -llibopencv_imgproc411 ^
+                -llibopencv_ml411 ^
+                -llibopencv_objdetect411 ^
+                -llibopencv_photo411 ^
+                -llibopencv_stitching411 ^
+                -llibopencv_video411 ^
+                -llibopencv_videoio411
